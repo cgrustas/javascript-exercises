919c8e847e8f3be1e1173367b98708559adaef36
Coleman Grustas <chgrustas@gmail.com>
2025-06-04 17:54:08 -0400

    Complete Exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        while (arr.includes(item)) {
+            let index = arr.indexOf(item);
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
