399fe0631d61dc66fb1c577986126db2828f636a
Coleman Grustas <chgrustas@gmail.com>
2025-06-04 18:06:11 -0400

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let sum = 0;
 
+    if (x < 0 || y < 0) return 'ERROR';
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return 'ERROR';
+
+    let large = Math.max(x, y);
+    let small = Math.min(x, y);
+    for (let i = small; i <= large; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
