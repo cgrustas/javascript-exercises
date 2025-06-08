effd12acd8eca8fbdb3db0b6eb5c83dee1defa69
Coleman Grustas <chgrustas@gmail.com>
2025-06-08 14:18:47 -0400

    Complete Exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    if (member < 0) return "OOPS";
+    else if (member == 0) return 0;
+    else if (member == 1 || member == 2) return 1;
+    else {
+        return fibonacci(member - 1) + fibonacci(member - 2);
+    }
 
 };
 
