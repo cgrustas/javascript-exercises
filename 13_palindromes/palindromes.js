c5df8a072faf2c51067aa083804c35ed4b0c0513
Coleman Grustas <chgrustas@gmail.com>
2025-06-08 14:11:31 -0400

    Complete Exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const lowerCaseStr = str.toLowerCase();
+    let alphanumericStr = '';
+    for (i = 0; i < lowerCaseStr.length; i++) {
+        if (alphanumerical.includes(lowerCaseStr.charAt(i))) {
+            alphanumericStr += lowerCaseStr.charAt(i);
+        }
+    }
+    const reversedStr = alphanumericStr.split('').reverse().join('');
+    return alphanumericStr === reversedStr;
 };
 
 // Do not edit below this line
