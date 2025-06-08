9c17ba78b2334195af6d107f0fd33693c921c42d
Coleman Grustas <chgrustas@gmail.com>
2025-06-08 14:37:40 -0400

    Complete Exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((person, oldest) => age(person) > age(oldest) ? person : oldest, 
+        people[0]);
 };
+
+function age(person) {
+    if (!('yearOfDeath' in person)) person['yearOfDeath'] = new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
